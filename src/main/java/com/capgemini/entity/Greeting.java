cb2a20304b69e7ba3a6e7a570a3a2cd8ee0cb7f4
dsanimm <dsanimm@gmail.com>
2020-11-25 07:18:24 +0530

    greetings app controller is defined

---
+++ src/main/java/com/capgemini/entity/Greeting.java
@@ -0,0 +1,23 @@
+package com.capgemini.entity;
+
+public class Greeting {
+public Greeting(long id, String message) {
+		super();
+		this.id = id;
+		this.message = message;
+	}
+long id;
+String message;
+public long getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getMessage() {
+	return message;
+}
+public void setMessage(String message) {
+	this.message = message;
+}
+}
